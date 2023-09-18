76510871ff9f9fdf87ec1ad80b4f5113de5c7298
dhvani-gupta <63143690+dhvani-gupta@users.noreply.github.com>
2023-09-18 19:38:50 +0530

    Change in script.js

--- Portfolio-Website/assets/js/script.js
+++ Portfolio-Website/assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["Product Management", "Backend Development", "Web Designing", "Android Development", "Software Development","Data Science"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
